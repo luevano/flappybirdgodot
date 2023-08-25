02517308e31c47d09ea43c68d8e2b81ad6f690d6
David Luevano Alvarado <david@luevano.xyz>
2023-08-24 23:47:37 -0600

    chore: refactor ui and elements, remove fps monitoring

--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -1,8 +1,6 @@
 class_name UI
 extends CanvasLayer
 
-
-@export var fps_label: Label
 @export var version_label: Label
 @export var score_label: Label
 @export var high_score_label: Label
@@ -12,23 +10,12 @@ extends CanvasLayer
 
 
 func _ready() -> void:
-	fps_label.visible = false
 	version_label.set_text("v%s" % Global.VERSION)
 	high_score_label.set_text("High score: %s" % _initial_high_score)
 
 	Event.game_start.connect(_on_game_start)
 	Event.game_over.connect(_on_game_over)
 	Event.new_score.connect(_on_new_score)
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("toggle_debug"):
-		fps_label.visible = !fps_label.visible
-
-
-func _process(delta: float) -> void:
-	if fps_label.visible:
-		fps_label.set_text("FPS: %d" % Performance.get_monitor(Performance.TIME_FPS))
 
 
 func _on_game_start() -> void:
